b63086a195b60ef70a14cbc1946a686d44158e41
Harishchandra Gandhare <harishchandragandhare@gmail.com>
2025-09-01 21:12:05 +0530

    docs: add CONTRIBUTING.md with contribution guidelines

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,8 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from "./schema"
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
 
-const postgresql = neon(process.env.NEXT_PUBLIC_DATABASE_URL!);
+// server-side environment variable
+const postgresql = neon(process.env.DATABASE_URL!);
+
 export const db = drizzle(postgresql, { schema });
-
